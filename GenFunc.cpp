09ec32795a097f5e6a9adbee21da10ec0a023391
shahafMordechay <36819025+shahafMordechay@users.noreply.github.com>
2018-12-22 23:35:46 +0200

    updated isNumber function
    updated isNumber function to identify fractions

--- GenFunc.cpp
+++ GenFunc.cpp
@@ -11,9 +11,18 @@ bool GenFunc::isOperator(char sign) {
 
 // true if given string is a number, false otherwise
 bool GenFunc::isNumber(string &str) {
+    bool hasOneDigit = false;
     for (int i = 0; i < str.length(); ++i) {
-        if (!isdigit(str[i]) && (str.length() == 1 || i != 0 || str[0] != '-')) {
+        if (!isdigit(str[i]) && str[i] != '.' && (str.length() == 1 || i != 0 || str[0] != '-')) {
             return false;
+        }
+
+        if (hasOneDigit && str[i] == '.') {
+            return false;
+        }
+
+        if (str[i] == '.') {
+            hasOneDigit = true;
         }
     }
 
