4ccc78f01580bb4ab9fc513bf2ddd3ee0e852af5
shahafMordechay <36819025+shahafMordechay@users.noreply.github.com>
2018-12-27 00:12:28 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/flightSimulator.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shahaf/CLionProjects/flightSimulator/GenFunc.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/GenFunc.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/AssertionCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/AssertionCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/CommandExpression.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/CommandExpression.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/ConditionParser.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/ConditionParser.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/ConnectCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/ConnectCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/DefineVarCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/DefineVarCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/IfCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/IfCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/Lexer.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/Lexer.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/LoopCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/LoopCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/OpenServerCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/OpenServerCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/PrintCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/PrintCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/ReadFromServer.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/ReadFromServer.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/SleepCommand.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/SleepCommand.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/commands/commandsFactory.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/commands/commandsFactory.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/expressions/BinaryExpression.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/expressions/BinaryExpression.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/expressions/Div.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/expressions/Div.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/expressions/MathExpCalc.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/expressions/MathExpCalc.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/expressions/Minus.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/expressions/Minus.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/expressions/Mult.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/expressions/Mult.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/expressions/Number.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/expressions/Number.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/expressions/Plus.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/expressions/Plus.cpp.o"
+  "/home/shahaf/CLionProjects/flightSimulator/main.cpp" "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/CMakeFiles/flightSimulator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/flightSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flightSimulator.dir/main.cpp.o"
+  "CMakeFiles/flightSimulator.dir/expressions/BinaryExpression.cpp.o"
+  "CMakeFiles/flightSimulator.dir/expressions/Number.cpp.o"
+  "CMakeFiles/flightSimulator.dir/expressions/Plus.cpp.o"
+  "CMakeFiles/flightSimulator.dir/expressions/Minus.cpp.o"
+  "CMakeFiles/flightSimulator.dir/expressions/Mult.cpp.o"
+  "CMakeFiles/flightSimulator.dir/expressions/Div.cpp.o"
+  "CMakeFiles/flightSimulator.dir/expressions/MathExpCalc.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/OpenServerCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/Lexer.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/LoopCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/DefineVarCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/ConnectCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/ConditionParser.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/IfCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/PrintCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/SleepCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/AssertionCommand.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/CommandExpression.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/commandsFactory.cpp.o"
+  "CMakeFiles/flightSimulator.dir/commands/ReadFromServer.cpp.o"
+  "CMakeFiles/flightSimulator.dir/GenFunc.cpp.o"
+  "flightSimulator.pdb"
+  "flightSimulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flightSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
