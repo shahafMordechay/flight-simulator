84a3e3d599c877b639f5779519bd31c82d5a67ae
Bknisan <39652860+Bknisan@users.noreply.github.com>
2018-12-27 00:47:38 +0200

    final version

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/shahaf/CLionProjects/flightSimulator
+# Install script for directory: /home/nisan/CLionProjects/flightSimulator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/shahaf/CLionProjects/flightSimulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nisan/CLionProjects/flightSimulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
