7c9ae47dc3f37c2d3be297e2638e2a35bda05d08
Bknisan <39652860+Bknisan@users.noreply.github.com>
2019-01-14 14:25:34 +0200

    test main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-#include <string.h>
-#include <queue>
-#include "expressions/MathExpCalc.h"
-#include "commands/Lexer.h"
-using namespace std;
+#include <vector>
+#include "./problems/Matrix.h"
+#include "algorithms/BFS.h"
+#include "algorithms/DFS.h"
 
-int main(int argc, char *argv[]) {
-    auto *myreader = new Lexer(argc, argv);
-    myreader->parser(myreader->lexer(), 0);
-    delete(myreader);
+int main() {
+    std::string src = "3,3";
+    std::string dst = "0,0";
+    std::vector<std::string> maze;
+    maze.emplace_back("1,3,6,2");
+    maze.emplace_back("1,2,1,2");
+    maze.emplace_back("1,3,9,2");
+    maze.emplace_back("1,5,6,7");
+    Matrix *myOne = new Matrix(maze, src, dst);
+    Searcher<Entry, string> *mySearcher = new BFS <string>();
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
