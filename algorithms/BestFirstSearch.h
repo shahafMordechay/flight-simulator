1926a713bd943e005970a27aa2f2682a1694aacf
shahafMordechay <36819025+shahafMordechay@users.noreply.github.com>
2019-01-15 14:30:38 +0200

    BestFirstSearch.h

--- algorithms/BestFirstSearch.h
+++ algorithms/BestFirstSearch.h
@@ -1,7 +1,3 @@
-//
-// Created by nisan on 1/12/19.
-//
-
 #ifndef FLIGHTSIMULATOR_BESTFIRSTSEARCH_H
 #define FLIGHTSIMULATOR_BESTFIRSTSEARCH_H
 
@@ -10,14 +6,92 @@
 #include "Searcher.h"
 template <class Solution>
 class BestFirstSearch: public Searcher<class Entry, Solution>{
-    //pr queue.
     priority_queue<State<Entry>> movingBest;
-    virtual State<Entry> popOpenList();
-    virtual int openListSize();
-    virtual bool exists(State<Entry>);
-    string backTrace(State<Entry>);
-    virtual string search(ISearchable<Solution> searchable);
-    BestFirstSearch();
+
+    virtual State<Entry> popOpenList() {
+        this->evaluatedNodes++;
+        State<Entry> front = this->movingBreath.front();
+        this->movingBreath.pop();
+        return front;
+    }
+
+    virtual int openListSize() {
+        return (int) this->movingBest.size();
+    }
+
+    virtual bool exists(State<Entry> wanted) {
+        // make copy of my queue.
+        queue<State<Entry>> copy = this->movingDeep;
+        // while still contains elements keep pop.
+        while (!copy.empty()) {
+            // if exists.
+            if (wanted.isSameState(copy.front()))
+                return true;
+            copy.pop();
+        }
+        //no element equals.
+        return false;
+    }
+
+    string backTrace(State<Entry> target) {
+        string mySol = "";
+        while (target.getCameFrom() != NULL) {
+            // concat string
+            mySol = target.getState().fromWhere(target.getCameFrom()->getState()) + ", " + mySol;
+            // go back.
+            target = *target.getCameFrom();
+        }
+        // cut last ", "
+        mySol = mySol.substr(0, mySol.length() - 3);
+        return mySol;
+    }
+
+public:
+    BestFirstSearch() {
+        this->movingBest = priority_queue<State<Entry>>();
+    }
+
+    virtual string search(ISearchable<Solution> searchable) {
+
+        //push first
+        this->movingBest.push(searchable.getInitialState());
+
+        //already visited set
+        unordered_set<State<Entry>> closed;
+
+        //still entries to check
+        while (openListSize() > 0) {
+
+            //get first in the queue
+            State<Entry> current = popOpenList();
+
+            // mark as visited
+            closed.insert(current);
+
+            // current is target
+            if (current.isSameState(searchable.getGoalState())) {
+                return backTrace(current);
+            }
+
+            // all possible states from current
+            list<State<Entry>> successors = searchable.getAllPossibleStates(current);
+
+
+            for (State<Entry> successor : successors) {
+                if (closed.find(successor) == closed.end() && !exists(successor)) {
+                    successor.setCameFrom(&current);
+                    this->movingBest.push(successor);
+                } else if (successor.getCost() > current.getCost() + 1) {
+                    if (!exists(successor)) {
+                        successor.setCameFrom(&current);
+                        this->movingBest.push(successor);
+                    } else {
+                        successor.setCost(current.getCost() + 1);
+                    }
+                }
+            }
+        }
+    }
 };
 
 
